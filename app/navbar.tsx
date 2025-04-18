336e5c199501b621ad661f95ec1ec0e9f52b3d0a
Maksym Symonov <ssmaksss33@gmail.com>
2025-04-18 16:10:50 +0300

    scroll up

--- app/navbar.tsx
+++ app/navbar.tsx
@@ -295,7 +295,7 @@
 
 import { useEffect, useState } from 'react';
 import Link from 'next/link';
-import { ShoppingCartIcon } from "lucide-react";
+import {ShoppingCartIcon, StoreIcon} from "lucide-react";
 
 export default function Navbar() {
     const [cartCount, setCartCount] = useState(0);
@@ -322,8 +322,16 @@ export default function Navbar() {
 
         <nav className="bg-gray-800 text-white p-4 fixed top-0 left-0 w-full z-50 shadow-md backdrop-blur-sm">
             <div className="flex items-center justify-center md:justify-between">
-                <Link href="/" className="text-xl font-bold flex-1 text-center md:text-left">
-                    Головна
+                {/*<Link href="/" className="text-xl font-bold flex-1 text-center md:text-left">*/}
+                {/*    Головна*/}
+                {/*</Link>*/}
+                <Link
+                    href="/"
+                    className="flex items-center justify-center gap-2 flex-1 md:justify-start group transition-transform"
+                >
+                    <div className="p-2 rounded-full bg-gray-700 group-hover:bg-blue-600 transition-all duration-300 transform group-hover:scale-110 group-hover:rotate-[360deg] shadow-md">
+                        <StoreIcon className="h-6 w-6 text-white" />
+                    </div>
                 </Link>
                 <Link href="/cart" className="relative px-4 py-2 bg-blue-500 rounded-md hover:bg-blue-600 flex items-center gap-2">
                     <span>Корзина</span>
